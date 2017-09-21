00dcc1b6ccfad81caa8ef01fd935d21485ee8a01
JacksChoco <ljs8860@snu.ac.kr>
2017-09-21 22:06:06 +0900

    child 라우트

---
+++ src/components/FirstRoute.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="Hello">
+        <h1>{{msg}}</h1>
+        Hello {{ $route.params.name}}
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'FirstRoute',
+        data () {
+            return {
+                msg: 'FirstRoute'
+            }
+        }
+    }
+</script>
---
+++ src/components/FirstRouteChild.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="Hello">
+        <h1>{{msg}}</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'FristRouteChild',
+        data() {
+            return {
+                msg: 'FirstRouteChild'
+            }
+        }
+    }
+</script>
